b1fe21569e4a5ddcbb01bdf973ac4d946c8b0cb6
Sanya <sanyaivlitvyak@gmail.com>
2018-10-17 15:54:24 +0200

    Comments and cleanup in WeepingAngel script

--- Assets/Scripts/WeepingAngel.cs
+++ Assets/Scripts/WeepingAngel.cs
@@ -23,9 +23,8 @@ public class WeepingAngel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        CheckIfVisible();
 
-        if (!visible)
+        if (CheckIfVisible())
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
@@ -33,24 +32,25 @@ public class WeepingAngel : MonoBehaviour {
 
     private bool CheckIfVisible()
     {
-        if (_rend.isVisible)
+        if (_rend.isVisible) //Check if Unity thinks the renderer is visible (Not perfect but works as a quick and easy out in case it's not)
         {
-            if (Vector3.Dot((_cam.transform.position - transform.position).normalized, _cam.transform.forward) > Mathf.Lerp(-0.6f, -0.25f, Vector3.Distance(_cam.transform.position, transform.position) / 10))
-                return visible = false;
+            if (Vector3.Dot((_cam.transform.position - transform.position).normalized, _cam.transform.forward) > Mathf.Lerp(-0.6f, -0.25f, Vector3.Distance(_cam.transform.position, transform.position) / 10)) //Bad attempt at checking if the player is looking towards the enemy through the dot products of directions
+                return visible = false; //Set visible to false and return visible (which is false)
 
             int taskNumber = Time.frameCount % colls.Length; //Run one box per frame
-            if (taskNumber == 0)
-                visible = false;
-            if (visible)
-                return true;
 
-            _planes = GeometryUtility.CalculateFrustumPlanes(_cam);
+            if (taskNumber == 0)
+                visible = false; //Reset visibility status at start of the "loop"
+            if (visible)
+                return true; //If visible is true then just return since it means the enemy has been seen for this round of the "loop"
+
+            _planes = GeometryUtility.CalculateFrustumPlanes(_cam); //Get the frustum planes of the camera
             
-            if (GeometryUtility.TestPlanesAABB(_planes, colls[taskNumber].bounds))
+            if (GeometryUtility.TestPlanesAABB(_planes, colls[taskNumber].bounds)) //Test if the current boxcollider is within the camera's frustum
             {
-                BoxCollider coll = colls[taskNumber];
+                BoxCollider coll = colls[taskNumber]; //Get the current boxcollider
                 _points[0] = transform.TransformPoint(colls[taskNumber].center);
-                _points[1] = transform.TransformPoint(colls[taskNumber].center + new Vector3(coll.size.x, -coll.size.y, coll.size.z) * 0.5f);
+                _points[1] = transform.TransformPoint(colls[taskNumber].center + new Vector3(coll.size.x, -coll.size.y, coll.size.z) * 0.5f); //One corner of the boxcollider
                 _points[2] = transform.TransformPoint(colls[taskNumber].center + new Vector3(coll.size.x, -coll.size.y, -coll.size.z) * 0.5f);
                 _points[3] = transform.TransformPoint(colls[taskNumber].center + new Vector3(-coll.size.x, -coll.size.y, coll.size.z) * 0.5f);
                 _points[4] = transform.TransformPoint(colls[taskNumber].center + new Vector3(-coll.size.x, -coll.size.y, -coll.size.z) * 0.5f);
@@ -59,15 +59,12 @@ public class WeepingAngel : MonoBehaviour {
                 _points[7] = transform.TransformPoint(colls[taskNumber].center + new Vector3(-coll.size.x, coll.size.y, coll.size.z) * 0.5f);
                 _points[8] = transform.TransformPoint(colls[taskNumber].center + new Vector3(-coll.size.x, coll.size.y, -coll.size.z) * 0.5f);
 
-                foreach (Vector3 point in _points)
-                    if (!Physics.Linecast(point, _cam.transform.position, ignoreLayers)) {
-                        visible = true;
-                        return true;
-                    }
-                        
+                foreach (Vector3 point in _points) //Loop through the points array
+                    if (!Physics.Linecast(point, _cam.transform.position, ignoreLayers)) //Linecast between the enemy and the camera to check if there is anything in the way
+                        return visible = true; //If there is nothing in the way then the enemy is visible, so set visible to true and return it.        
             }
         }
 
-        return false;
+        return visible = false; //Set visible to false and return it
     }
 }
