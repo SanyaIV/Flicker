31b1c6b33e5330ca47ad777f74ddb95ec325dd4f
Sanya <sanyaivlitvyak@gmail.com>
2018-10-19 17:18:00 +0200

    Switched Post-Process plugin from asset store to Package Manager, because newer, added link to sanity for vignette

--- Assets/Scripts/Sanity/Sanity.cs
+++ Assets/Scripts/Sanity/Sanity.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Sanity : MonoBehaviour {
 
@@ -10,9 +11,20 @@ public class Sanity : MonoBehaviour {
     [SerializeField] private float _refillRate = 0.01f;
     private float _sanity = 1f;
 
+    [Header("Vignette")]
+    [SerializeField] private PostProcessVolume _ppVolume;
+    private Vignette _vignette;
+
     public void Start()
     {
         _sanity = _sanityRange.Max;
+
+        _vignette = ScriptableObject.CreateInstance<Vignette>();
+        _vignette.enabled.Override(true);
+        _vignette.intensity.Override(0f);
+        _vignette.smoothness.Override(1f);
+
+        _ppVolume = PostProcessManager.instance.QuickVolume(LayerMask.NameToLayer("PostProcessing"), 100f, _vignette);
     }
 
     public float GetSanity()
@@ -28,6 +40,8 @@ public class Sanity : MonoBehaviour {
             _sanity = _sanityRange.Min;
         /*if (_sanity == _sanityRange.Min)
             Death();*/
+
+        UpdateVignette();
     }
 
     public void RefillSanity(float multiplier = 1f)
@@ -36,10 +50,24 @@ public class Sanity : MonoBehaviour {
             _sanity += _refillRate * multiplier * Time.deltaTime;
         if (_sanity > _sanityRange.Max)
             _sanity = _sanityRange.Max;
+
+        UpdateVignette();
     }
 
     public void ResetSanity()
     {
         _sanity = _sanityRange.Max;
+
+        UpdateVignette();
+    }
+
+    private void UpdateVignette()
+    {
+        _vignette.intensity.value = Mathf.Lerp(1f, 0f, _sanity / (_sanityRange.Max - _sanityRange.Min));
+    }
+
+    void Destroy()
+    {
+        RuntimeUtilities.DestroyVolume(_ppVolume, true);
     }
 }
