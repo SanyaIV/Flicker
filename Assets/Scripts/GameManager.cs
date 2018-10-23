d3d97d3a8864f891779e68dc69fc148a2fa0b3a9
todorowski <emma.t.wahrenberg@gmail.com>
2018-10-23 13:32:56 +0200

    Enemy controller and GameManager script for save and respawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private Transform _player;
+    private Vector3 _savedPlayerPos;
+
+    public void Start()
+    {
+        _player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    void respawn()
+    {
+        _player.position = _savedPlayerPos;
+    }
+
+    void save()
+    {
+        _savedPlayerPos = _player.position;
+
+    }
+}
