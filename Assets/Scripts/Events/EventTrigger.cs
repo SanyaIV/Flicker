316f40ac9f4e752c0781a3249891a73ac0af7089
Sanya <sanyaivlitvyak@gmail.com>
2018-11-16 17:40:43 +0100

    Added wait time

--- Assets/Scripts/Events/EventTrigger.cs
+++ Assets/Scripts/Events/EventTrigger.cs
@@ -10,19 +10,33 @@ public class EventTrigger : MonoBehaviour {
     [SerializeField] private string _triggerTag = "Player";
     [SerializeField] [Range(0, 100)] private int _percentTriggerChance;
     [SerializeField] private int _maxTriggerTimes;
+    [SerializeField] private float _minWaitBetweenTriggersTime;
     [SerializeField] private UnityEvent _event;
     private int _triggerTimes;
+    private bool _waiting = false;
 
     void Trigger()
     {
+        if (_triggerTimes >= _maxTriggerTimes || _waiting)
+            return;
+
         _event.Invoke();
         _triggerTimes++;
+        StartCoroutine(Wait());
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (_triggerTimes < _maxTriggerTimes && other.tag == _triggerTag)
+        if (other.tag == _triggerTag)
             if (Random.Range(1, 101) <= _percentTriggerChance)
                 Trigger();
+    }
+
+    IEnumerator Wait()
+    {
+        _waiting = true;
+        yield return new WaitForSeconds(_minWaitBetweenTriggersTime);
+        _waiting = false;
+        yield break;
     }
 }
