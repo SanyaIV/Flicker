e5038b29d6f76fa4487798adbd6ba82d6f54be12
Sanya <sanyaivlitvyak@gmail.com>
2018-12-13 15:55:39 +0100

    Some last minute fixes

---
+++ Assets/Scripts/UI/Pulsate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulsate : MonoBehaviour
+{
+    [SerializeField] private CanvasRenderer _canvasRenderer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
