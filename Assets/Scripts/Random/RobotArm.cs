54a8490525d32d19a59356a6171e340094236e22
Sanya <sanyaivlitvyak@gmail.com>
2018-12-13 23:56:50 +0100

    Uhh.. Guess I worked like 15 hours today

---
+++ Assets/Scripts/Random/RobotArm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotArm : MonoBehaviour
+{
+    private Vector3 _lastPosition;
+
+    void LateUpdate()
+    {
+        _lastPosition = transform.position;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            Vector3 direction = transform.position - _lastPosition;
+
+            coll.GetComponent<CharacterController>().Move(direction * 10f);
+        }
+    }
+}
